d73557ef53aec3348e80b54e478668c927262afe
Lucawinwin <windwehrluca@gmail.com>
2025-05-27 10:31:37 +0200

    ff

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,1 +1,8 @@
-test :
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
