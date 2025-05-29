c8aab32852192ef39ec43948d3f62c788f78c337
Lucawinwin <windwehrluca@gmail.com>
2025-05-29 14:17:18 +0200

    ddd

---
+++ TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Create a type RowID and set it equal to number
+type RowID = number;
+
+// Create an interface RowElement with the specified fields
+interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // Optional field indicated by the ?
+}
